77e517cc866c3ff8fc2c9ccb3808bab3f5a172f4
dkStephanos <stephanos@goldmail.etsu.edu>
2020-01-14 11:59:49 -0500

    Moved over old Resume version

--- portfolio-app/src/Components/Resume.js
+++ portfolio-app/src/Components/Resume.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'reactstrap';
+import '../Stylesheets/Resume.css';
 
 class Resume extends Component {
   render() {
     return (
       <Row>
         <Col>
-
-        <div dataAnimation="flip-in-x" style={{animationDelay: "0.6s"}}>
-        Resume
-        </div>
+<div className="resume">
+      <div className="content">
+      <a className="logo"></a>
+      <p>
+        I am a software engineer with a passion for innovative solutions. I have a bachlors of science with a philosophy major and computing minor and went on to graduate from Flatiron School's software engineering program. I recently accepted a position as a technical consultant with DigiDems to assist the Democratic party in their efforts to win back Congress in 2018. I am currently specializing in React, JavaScript, and Rails, and have a background in object-oriented programming with languages like Java and C#. I bring strong skills in agile application development, as well as an eagerness to develop skills that help driven companies enrich their programming culture and realize their specific goals.
+      </p>
+      <a href="..." className="downloadButton">DOWNLOAD RESUME</a>
+      </div>
+    </div>
         </Col>
       </Row>
     );
