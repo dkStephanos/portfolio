1505aaf77f8c6162981da99873fa9a54a994e8c7
dkStephanos <stephanos@goldmail.etsu.edu>
2020-01-14 12:03:49 -0500

    Moved over old portfolio version as projects

--- portfolio-app/src/Components/Projects.js
+++ portfolio-app/src/Components/Projects.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'reactstrap';
+import '../Stylesheets/Projects.css';
 
 class Projects extends Component {
   render() {
@@ -7,9 +8,17 @@ class Projects extends Component {
       <Row>
         <Col>
 
-        <div dataAnimation="flip-in-x" style={{animationDelay: "0.6s"}}>
-        Projects
-        </div>
+        <section className="projects">
+			<div className="content">
+				<h2><b>01</b>Projects</h2>
+				<h3>My Latest Work. <a href="">See More &gt;</a></h3>
+				<div className="gallery">
+					<a href="https://stephanos.pagekite.me" className="fantasyPuntAnalyzer" target="_blank"></a>
+					<a href="https://github.com/dkStephanos/anti-social-network-client" className="antiSocialNetwork" target="_blank"></a>
+					<a href="https://github.com/dkStephanos/albumOrganizer" className="albumOrganizer" target="_blank"></a>
+				</div>
+			</div>
+		</section>
         </Col>
       </Row>
     );
