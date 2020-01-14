d8c3d5bfbc839c7e12fa5324d085f6a9573a8770
dkStephanos <stephanos@goldmail.etsu.edu>
2020-01-14 11:57:11 -0500

    Moved over old version of contact

--- portfolio-app/src/Components/Contact.js
+++ portfolio-app/src/Components/Contact.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'reactstrap';
+import '../Stylesheets/Contact.css';
 
 class Contact extends Component {
   render() {
     return (
       <Row>
         <Col>
-
-        <div dataAnimation="flip-in-x" style={{animationDelay: "0.6s"}}>
-        Contact
+        <div className="contact">
+        <div className="content">
+        <a className="email" href="mailto:dkstephanos@gmail.com">dkstephanos@gmail.com</a>
+        <div className="socialbar">
+          <a href="https://www.linkedin.com/in/dkstephanos/" title="Linkedin" target="_blank"><i className="fab fa-linkedin"></i></a>
+          <a href="https://github.com/dkStephanos" title="Github"target="_blank"><i className="fab fa-github"></i></a>
+          <a href="https://stephanossolutions.com" title="Tech Blog" target="_blank"><i className="fas fa-universal-access"></i></a>
         </div>
+      </div>
+      </div>
         </Col>
       </Row>
     );
